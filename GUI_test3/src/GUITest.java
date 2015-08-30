f3666cec0236bde525f65a5e1b97035721eafe18
lssxfy123 <liushenshenxfy@126.com>
2015-08-30 18:56:11 +0800

    Java学习，图像界面基础

---
+++ GUI_test3/src/GUITest.java
@@ -0,0 +1,33 @@
+
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class GUITest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ShowFlowLayout frame = new ShowFlowLayout();
+		frame.setTitle("ShowFlowLayout");
+		frame.setSize(200, 200);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+
+}
+
+class ShowFlowLayout extends JFrame {
+	public ShowFlowLayout() {
+		setLayout(new FlowLayout(FlowLayout.LEADING, 10, 20));
+		
+		add(new JLabel("First Name"));
+		add(new JTextField(8));
+		add(new JLabel("MI"));
+		add(new JTextField(1));
+		add(new JLabel("Last Name"));
+		add(new JTextField(8));
+	}
+}
